24f948439ed0c35eef55784275ed3c3fa96ae21b
prince475 <danielmwai475@gmail.com>
2023-03-03 20:02:24 +0300

    Completed orms mapping

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,15 @@
 require 'bundler'
 Bundler.require
 
+
 require_relative '../lib/song'
 
-DB = { conn: SQLite3::Database.new("db/music.db") }
+# setting up a constant DB equal to a hash that contains the connection to the database
+DB = { conn: SQLite3::Database.new('db/music.db')}
+
+# require 'bundler'
+# Bundler.require
+
+# require_relative '../lib/song'
+
+# DB = { conn: SQLite3::Database.new("db/music.db") }
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,46 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  # using the save method to insert data to our db using a heredoc to craft our multi-line SQL statement and using bound parameters.
+  # Bound parameters protect our program from getting confused be SQL injections and special characters....using ? as place holders.
+
+  def save
+    sql = <<-SQL
+    INSERT INTO songs (name, album)
+    VALUES (?, ?)
+    SQL
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.album)
+
+    # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+     # return the Ruby instance
+     self
+
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
   end
 
 end
